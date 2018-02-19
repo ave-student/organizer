ea0a30d94b3c590d31b6868319d089f3b2c8d30f
ave-student <free-is@ya.ru>
2018-02-19 18:37:32 +0500

    First function

--- js/organizer.js
+++ js/organizer.js
@@ -1,3 +1,16 @@
 $(function() {
-  $("#main").text("This is text from js-script...");
+	function TableModel(rows, cols) {
+		this._rows = rows;
+		this._cols = cols;
+	}
+	
+	TableModel.prototype.setRows = function(rows) {
+		this._rows = rows;
+	}
+	
+	TableModel.prototype.getRows = function() {
+		return this._rows;
+	}
+	
+	$("#main").text("This is text from js-script...");
 })
